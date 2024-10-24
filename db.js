c941eb2fc10d82724f013aed4356ee43164afc95
piyusdev2006 <piyushsingh955583@gmail.com>
2024-10-24 12:21:35 +0530

    shift local mongodb server to mongodb atlas online

--- db.js
+++ db.js
@@ -2,10 +2,13 @@
 // responnsible for connecting to the database
 
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 // Define the MongoDB connection url
 
-const mongoURL = "mongodb://localhost:27017/hotels";
+// const mongoURL = process.env.MONGODB_URL_LOCAL;
+
+const mongoURL = process.env.MONGODB_URL;
 
 // set up MongoDB connection string
 
--- server.js
+++ server.js
@@ -5,9 +5,11 @@
 const express = require("express");
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("Building my first web server with Node.js");
@@ -46,8 +48,10 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 app.use("/menuItem", menuItemRoutes);
 
 
+
+
 // here the server listen to port 3000
-app.listen(3000 , () => {
+app.listen(PORT , () => {
     console.log("\n Server started on port 3000");
 });
 
